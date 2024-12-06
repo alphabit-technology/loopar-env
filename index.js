8e4566425b3e01b274ec3ae51e42864f6a000984
Alfredo <alfredrz2012@gmail.com>
2024-12-05 22:26:12 -0500

    Update server configuration handling and bump version to 2.0.5

--- index.js
+++ index.js
@@ -38,11 +38,23 @@ program
       }
 
       // Step 4: Update the configuration file with the selected port (if applicable)
-      const configPath = path.join(targetPath, 'config.js'); // Adjust this path based on your project
+      const configPath = path.join(targetPath, 'config', 'server.config.json'); // Adjust this path based on your project
       if (fs.existsSync(configPath)) {
         let config = fs.readFileSync(configPath, 'utf-8');
         config = config.replace(/port:\s*\d+/i, `port: ${port}`); // Adjust based on your file format
         fs.writeFileSync(configPath, config, 'utf-8');
+        console.log(`Configuration updated with port ${port}.`);
+      }else{
+        fs.writeFileSync(path.join(targetPath, 'config', 'server.config.json'), JSON.stringify({
+          "port": port,
+          "session": {
+            "secret": "secrctekeyf5d665dd56ff59fbd24699e502a528f77eb786e8",
+            "saveUninitialized": false,
+            "cookie": { "maxAge": 86400000 },
+            "resave": false
+          }}, null, 2
+        ), 'utf-8');
+
         console.log(`Configuration updated with port ${port}.`);
       }
 
